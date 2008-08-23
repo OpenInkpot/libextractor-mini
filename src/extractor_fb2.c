4d6bb1c0bd408e943d845834a2dcc229917592a3
Mikhail Gusarov <dottedmag@dottedmag.net>
2008-08-24 03:41:38 +0700

    Proper copyright information is provided

--- src/extractor_fb2.c
+++ src/extractor_fb2.c
@@ -1,3 +1,25 @@
+/*
+ * extractor_fb2.c -- part of libextractor-oi-extras
+ *
+ * Part of OpenInkpot project (http://openinkpot.org/)
+ *
+ * (c) 2008 Marc Lajoie <quickhand@openinkpot.org>
+ * (c) 2008 Mikhail Gusarov <dottedmag@dottedmag.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <ctype.h>
 #include <errno.h>
 #include <expat.h>
